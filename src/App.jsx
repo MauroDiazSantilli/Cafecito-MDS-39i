81f0034a22349aad2d6e9475aafd88e81a2131b4
MauroDS <mauro.diaz.santilli@gmail.com>
2023-06-08 12:52:08 -0300

    Corrigiendo problema con swal. Falta agregar mas productos

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,28 @@ import CrearProducto from "./components/views/producto/CrearProducto";
 import EditarProducto from "./components/views/producto/EditarProducto";
 import Administrador from "./components/views/Administrador";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
+import RutasAdministrador from "./components/routes/RutasAdministrador";
+
 
 function App() {
+  const usuarioSessionStorage = JSON.parse(sessionStorage.getItem('usuario')) || {}
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioSessionStorage);
+
   return (
     <BrowserRouter>
-      <Menu></Menu>
+      <Menu usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Menu>
       <Routes>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
         <Route exact path="/registro" element={<Registro></Registro>}></Route>
-        <Route exact path="/login" element={<Login></Login>}></Route>
-        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
-        <Route exact path="/administrador/crear" element={<CrearProducto></CrearProducto>}></Route>
+        <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado} ></Login>}></Route>
+        <Route path="/administrador/*" element={
+          <RutasProtegidas>
+            <RutasAdministrador></RutasAdministrador>
+          </RutasProtegidas>
+        }></Route>
+       
         <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
       <Footer></Footer>
