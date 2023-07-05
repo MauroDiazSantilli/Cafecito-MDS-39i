13d0a08b0ed6bbb76cab8eb3834528e5f5e6b807
MauroDS <mauro.diaz.santilli@gmail.com>
2023-07-05 19:38:18 -0300

    Modificando banner

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -14,23 +14,25 @@ const Inicio = () => {
   },[])
 
   return (
-    <section className="mainSection">
-      <img
-        className="banner"
-        src="https://images.pexels.com/photos/6802983/pexels-photo-6802983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="fondo cafe"
-      />
-      <Container>
-        <h1 className="display-4">Nuestros Productos</h1>
-        <hr />
-        <Row>
-        {
-          productos.map((producto)=>  <CardProducto key={producto._id} producto={producto}></CardProducto>)
-        }
-        </Row>
-      </Container>
-    </section>
-  );
+    <main className="main">
+        <Container fluid className="p-0">
+            <Row>
+                <img src={imgBanner} alt="" className="imgBanner" />
+            </Row>
+        </Container>
+        <Container className="my-4">
+            <h1>Nuestros Productos</h1>
+            <hr />
+            <Row className="alineacionProductos">
+               {
+                productos.map((producto) => {
+                        return <CardProducto producto={producto} key={producto.id}></CardProducto>
+                })
+            }
+            </Row>
+        </Container>
+    </main>
+);
 };
 
 export default Inicio;
